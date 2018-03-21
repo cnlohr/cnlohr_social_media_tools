be97a90bc2804ca79e3228f8a15fbef28360acde
cnlohr <lohr85@gmail.com>
2018-03-21 15:12:13 -0400

    Add discord bridge.

---
+++ ytchatmonitor_discord_bridge/README.md
@@ -0,0 +1,73 @@
+Go make a webhook over at your discord server, under "Server settings" "Webhook"
+
+
+
+Just like ytchamonitor2, but add above...
+
+1. Set up a project here:
+	https://console.developers.google.com/cloud-resource-manager
+	 * Click: Enable APIs and get credentials like keys
+	 * Use youtube API v3
+	 * Click "Create credentials"
+	 * Select will be calling it from "Other UI"
+	 * Select Public data.
+	 * Get an API Key, and restrict by your IP address.
+	My restricted API key for this project is: 
+		AIzaSyA1XpoUMNDFOx0W4-HjiUI1uiahdfe20lE
+
+
+2. Visit your project here:
+	https://console.developers.google.com/home/dashboard?project=youtubecomments2
+
+3.  https://developers.google.com/youtube/v3/live/docs/liveBroadcasts/list
+      part = snippet / broadcstStatus = all / broadcstType = all
+
+	Use OAUTH on this site to get the liveChatId
+		EiEKGFVDRzd5SVd0VndjRU5nX1pTLW5haGc1ZxIFL2xpdmU
+
+
+	Then you can use API Key here..
+
+4. https://developers.google.com/youtube/v3/live/docs/liveChatMessages/list
+
+	curl "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=EiEKGFVDRzd5SVd0VndjRU5nX1pTLW5haGc1ZxIFL2xpdmU&part=snippet&key=AIzaSyA1XpoUMNDFOx0W4-HjiUI1uiahdfe20lE"
+
+	Totes works.
+
+```		{
+		 "kind": "youtube#liveChatMessageListResponse",
+		 "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/EktItOeGDW-qqheMbsHubaBnrS8\"",
+		 "nextPageToken": "GMq3wfej0dkCIJOw_uCl0dkC",
+		 "pollingIntervalMillis": 1526,
+		 "pageInfo": {
+		  "totalResults": 19,
+		  "resultsPerPage": 19
+		 },
+		 "items": [
+		  {
+		   "kind": "youtube#liveChatMessage",
+		   "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/zsQSWiW4xjZfucBiPw3dxnPgr4c\"",
+		   "id": "LCC.CiMSIQoYVUNHN3lJV3RWd2NFTmdfWlMtbmFoZzVnEgUvbGl2ZRI5ChpDT2ktXzhhZDBka0NGUkhVd1FvZENvRUt5dxIbQ0szM3VKNmMwZGtDRlFYQllBb2RKMWNNaFEw",
+		   "snippet": {
+			"type": "textMessageEvent",
+			"liveChatId": "EiEKGFVDRzd5SVd0VndjRU5nX1pTLW5haGc1ZxIFL2xpdmU",
+			"authorChannelId": "UCG7yIWtVwcENg_ZS-nahg5g",
+			"publishedAt": "2018-03-03T22:45:18.656Z",
+			"hasDisplayContent": true,
+			"displayMessage": "test",
+			"textMessageDetails": {
+			 "messageText": "test"
+			}
+		   }
+		  },```
+
+
+	Then, to get the author name: 
+		curl "https://www.googleapis.com/youtube/v3/channels?part=id,snippet,statistics,contentDetails,topicDetails&id=UCG7yIWtVwcENg_ZS-nahg5g&key=AIzaSyA1XpoUMNDFOx0W4-HjiUI1uiahdfe20lE"
+
+		curl "https://www.googleapis.com/youtube/v3/channels?part=id,snippet,statistics,contentDetails,topicDetails&id=UCCtuJQDZFMZJUdPX6Qxxc8g&key=AIzaSyA1XpoUMNDFOx0W4-HjiUI1uiahdfe20lE"
+
+
+abaltazar8 : its fine @CNLohr, the first request gets all chat history, to get updates, but after that you can use﻿ the nextPageToken to get the new messages instead of getting the whole thing
+
+
---
+++ ytchatmonitor_discord_bridge/discordbridge.c
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cnsslclient.h>
+#include <cnhttpclient.h>
+#include <jsmn.h>
+#include <unistd.h>
+
+int first = 1;
+char * NextPageToken = 0;
+	int pollinfo = 0;
+
+const char * discordtoken = 0;
+char discordurl[8192];
+char discorddata[8192];
+struct cnhttpclientrequest reqdiscord;
+
+const char * GetTokenByName( char * origtext, jsmntok_t * tok )
+{
+	origtext[tok->end] = 0;
+	return origtext + tok->start;
+}
+
+void ProcessNextElement( jsmntok_t ** tok )
+{
+	int nr_children = (*tok)->size;
+	int j;
+	//printf( "Process Next: %d\n", nr_children );
+	for( j = 0; j < nr_children; j++ )
+	{
+		(*tok)++;
+		ProcessNextElement( tok );
+	}
+}
+
+const char * ReadInsideChatSnippet( char * origtext, jsmntok_t ** tok )
+{
+	const char * msgo = 0;
+	int j;
+	int nr_children = (*tok)->size;
+
+	for( j = 0; j < nr_children; j++ )
+	{
+		(*tok)++;
+		const char * st = GetTokenByName( origtext, *tok );
+
+		if( strcmp( st, "displayMessage" ) == 0 )
+		{
+			msgo = GetTokenByName( origtext, (*tok) + 1 );
+			ProcessNextElement( tok );
+		}
+		else
+		{
+			ProcessNextElement( tok );
+		}
+	}
+	return msgo;
+}
+
+
+const char * ReadInsideChatAuthorDetails( char * origtext, jsmntok_t ** tok )
+{
+	const char * auth = 0;
+	int j;
+	int nr_children = (*tok)->size;
+
+	for( j = 0; j < nr_children; j++ )
+	{
+		(*tok)++;
+		const char * st = GetTokenByName( origtext, *tok );
+
+		if( strcmp( st, "displayName" ) == 0 )
+		{
+			auth = GetTokenByName( origtext, (*tok) + 1 );
+			ProcessNextElement( tok );
+		}
+
+		ProcessNextElement( tok );
+	}
+	return auth;
+}
+
+
+void ReadChatEntry( char * origtext, jsmntok_t ** tok, const char ** chatsnip, const char ** authorsnip )
+{
+	//Found items... Advnce to next token.
+	int j;
+	int nr_children = (*tok)->size;
+
+	for( j = 0; j < nr_children; j++ )
+	{
+		(*tok)++;
+		const char * st = GetTokenByName( origtext, *tok );
+
+		if( strcmp( st, "snippet" ) == 0 )
+		{
+			(*tok)++;
+			*chatsnip = ReadInsideChatSnippet( origtext, tok );
+		}
+		else if( strcmp( st, "authorDetails" ) == 0 )
+		{
+			(*tok)++;
+			*authorsnip = ReadInsideChatAuthorDetails( origtext, tok );
+		}
+		else
+		{
+			ProcessNextElement( tok );
+		}
+	}
+}
+
+
+void ProcessChatMessageResponse(char * origtext, jsmntok_t ** tok, jsmntok_t * tokend )
+{
+
+	int nr_children = (*tok)->size;
+	int j;
+	for( j = 0; j < nr_children; j++ )
+	{
+		(*tok)++;
+		const char * st = GetTokenByName( origtext, *tok );
+
+		if( strcmp( st, "pollingIntervalMillis" ) == 0 )
+		{			
+			const char * st = GetTokenByName( origtext, *tok + 1 );
+			pollinfo = atoi( st );
+		}
+		else if( strcmp( st, "nextPageToken" ) == 0 )
+		{
+			if( NextPageToken ) free( NextPageToken );
+			NextPageToken = strdup( GetTokenByName( origtext, *tok + 1 ) );
+		}
+		else if( strcmp( st, "items" ) == 0 )
+		{
+			(*tok)++;
+			int nr_children = (*tok)->size;
+			int i;
+			for( i = 0; i < nr_children; i++ )
+			{
+				(*tok)++;
+				const char * chatsnip, *authorsnip;
+				chatsnip = authorsnip = 0;
+				ReadChatEntry( origtext, tok, &chatsnip, &authorsnip );
+				printf( "%s: %s\n", authorsnip, chatsnip );
+				fflush( stdout );
+				if( !first )
+				{
+
+					//curl -X POST --data '{ "embeds": [{"title":"Test title", "description": "test text", "type": "rich", "color":"65535"}] }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/<ID>/<TOKEN>
+
+					sprintf( discordurl, "https://discordapp.com/api/webhooks/%s", discordtoken );
+					int len = sprintf( discorddata, "{ \"embeds\": [{\"title\":\"%s\", \"description\": \"%s\", \"type\": \"rich\", \"color\":\"65535\"}] }", authorsnip, chatsnip );
+					char addedh[1024];
+					sprintf( addedh, "Content-Type: application/json\r\nContent-length: %d", len );
+					reqdiscord.AddedHeaders = addedh;
+					reqdiscord.AuxDataLength = len;
+
+					struct cnhttpclientresponse * r = CNHTTPClientTransact( &reqdiscord );
+					r->payload[r->payloadlen-1] = 0;
+					CNHTTPClientCleanup( r );
+
+				}
+			}
+			if( nr_children ) first = 0;
+		}
+		ProcessNextElement( tok );
+	}
+}
+
+
+
+int main( int argc, char ** argv )
+{
+	char curlurl[8192];
+	jsmntok_t tokens[131072];
+	jsmn_parser jsmnp;
+
+	if( argc < 4 )
+	{
+		fprintf( stderr, "Error! Usage: ./chatmon [apikey] [livechatid] [discord id\"/\"token]\n" );
+		return -5;
+	}
+
+	const char * livechatid = argv[2];//"EiEKGFVDRzd5SVd0VndjRU5nX1pTLW5haGc1ZxIFL2xpdmU";
+	const char * apikey = argv[1];//"AIzaSyA1XpoUMNDFOx0W4-HjiUI1uiahdfe20lE";
+	const char * reqtype = "authorDetails,snippet";
+	discordtoken = argv[3];
+
+	memset( &reqdiscord, 0, sizeof( reqdiscord ) );
+	reqdiscord.host = 0;
+	reqdiscord.port = 0;
+	reqdiscord.URL = discordurl;
+	reqdiscord.AddedHeaders = "";
+	reqdiscord.AuxData = discorddata;
+	reqdiscord.AuxDataLength = 0;
+
+
+	struct cnhttpclientrequest req;
+	memset( &req, 0, sizeof( req ) );
+	req.host = 0;
+	req.port = 0;
+	req.URL = curlurl;
+	req.AddedHeaders = "";
+	req.AuxData = 0;
+	req.AuxDataLength = 0;
+
+	while( 1 )
+	{
+		sprintf( curlurl, "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=%s&part=%s&key=%s%s%s",
+			livechatid, 
+			reqtype,
+			apikey,
+			NextPageToken?"&pageToken=":"",
+			NextPageToken?NextPageToken:"" );
+
+		//printf("%s\n", curlurl );
+
+		//Get a bunch of messages
+		struct cnhttpclientresponse * r = CNHTTPClientTransact( &req );
+
+		r->payload[r->payloadlen-1] = 0;
+
+		//printf( "PAYLOAD: %s\n", r->payload );
+		jsmn_init( &jsmnp );
+		int tottoks = jsmn_parse(&jsmnp, r->payload, r->payloadlen,
+			tokens, sizeof(tokens)/sizeof(tokens[0]) );
+		if( tottoks > 0 )
+		{
+			jsmntok_t * tok = &tokens[0];
+			ProcessChatMessageResponse( r->payload, &tok, &tokens[tottoks] );
+			CNHTTPClientCleanup( r );
+		}
+		else
+		{
+			fprintf( stderr, "Error %d parsing the JSON : %s\n", tottoks, r->payload );
+			CNHTTPClientCleanup( r );
+		}
+
+		if( pollinfo )
+			usleep( pollinfo * 1000 );
+		else
+			sleep(2);
+	}
+
+}
+
